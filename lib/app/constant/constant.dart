eb14a817cf39b5ec03564f73e9acac02b1161c21
LeoHong123456 <zh4987758>
2024-07-28 23:38:42 +0800

    1

--- lib/app/constant/constant.dart
+++ lib/app/constant/constant.dart
@@ -3,6 +3,8 @@ import 'dart:ui';
 class Constant{
   ///API地址
   static const String BASE_URL = "http://103.143.80.82:9699/";
+  // static const String BASE_URL = "http://127.0.0.1:8088/";
+
   ///系统版本号
   static const String VERSION = "1.0.1";
 
--- lib/app/modules/loan_module/loan_controller.dart
+++ lib/app/modules/loan_module/loan_controller.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import '../../routes/app_pages.dart';
 import '../../theme/app_colors_theme.dart';
+import '../../utils/app_data.dart';
 import '../../utils/network/request_client.dart';
 
 
@@ -13,25 +17,37 @@ class LoanController extends GetxController{
   var loanAmount = ''.obs;
   var reason = ''.obs;
 
+  StreamSubscription? loginSub;
+  StreamSubscription? logoutSub;
+
   final formKey = GlobalKey<FormState>();
 
-  Future<void> submitForm() async {
-    if (formKey.currentState?.validate() ?? false) {
-      formKey.currentState?.save();
-
-      var data ={"realname": realname.value, "phone": phone.value, "identityId": identityId.value, "homeAddress": homeAddress.value, "homeRevenue": homeRevenue.value, "loanAmount": loanAmount.value, "reason": reason.value, "chatId": "1798367557139595266"};
-      var headers = {"version":"1.0.1", "Authorization": "hs5sl9xwkfhjgb39uhn3omee16cgqkiu"};
-      RequestClient requestClient = RequestClient();
-      var  user = await requestClient.post("loan/loanApplay", data: data, headers: headers);
-      debugPrint("-------------$user");
-      Get.snackbar("提交成功", "系统审批中！",snackPosition: SnackPosition.TOP,duration: const Duration(seconds: 1), backgroundColor: AppStyles.primaryColor, colorText: AppStyles.textWhiteColor);
-      // final tabsController = Get.find<TabsController>();
-      // tabsController.refresh();
-      // Get.toNamed(Routes.TABS);
-      // Get.back(closeOverlays: true);
-      Get.close(1);
-      update();
+  Future<void> submitForm() async{
+      if (formKey.currentState?.validate() ?? false) {
+        formKey.currentState?.save();
+        var userInfo = AppData.getUser();
+        if(isNotEmpty(userInfo)){
+          var headers = {"version":"1.0.1", "Authorization": userInfo?.token};
+          var data ={
+            "realname": realname.value,
+            "phone": phone.value,
+            "identityId": identityId.value,
+            "homeAddress": homeAddress.value,
+            "homeRevenue": homeRevenue.value,
+            "loanAmount": loanAmount.value,
+            "reason": reason.value,
+            "chatNo": userInfo?.chatNo,
+            "userId": userInfo?.userId
+          };
+          RequestClient requestClient = RequestClient();
+          var  user = await requestClient.post("loan/loanApplay", data: data, headers: headers);
+          debugPrint("-------------$user");
+          Get.snackbar("提交成功", "系统审批中！",snackPosition: SnackPosition.TOP,duration: const Duration(seconds: 1), backgroundColor: AppStyles.primaryColor, colorText: AppStyles.textWhiteColor);
+          Get.close(1);
+          update();
+      }else{
+          Get.toNamed(Routes.LOGIN);
+      }
     }
   }
-
 }
